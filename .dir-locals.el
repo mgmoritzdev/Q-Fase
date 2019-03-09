86a38a8e9fae1c3713173ff1bb950e0632b208a2
Marcos Moritz <moritz@portaltelemedicina.com.br>
2019-03-09 16:58:32 -0300

    Add emacs dir locals

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-compilation-cmd . "python main.py")
+         (projectile-project-test-cmd . "python shuffle.test.py")
+         (projectile-project-run-cmd . "python main.py"))))
