79a1ed722832493a1eb3a98192c149dda5246cb6
Marcos Moritz <moritz@portaltelemedicina.com.br>
2019-03-10 09:57:54 -0300

    implement shuffle

--- shuffle.py
+++ shuffle.py
@@ -1,5 +1,8 @@
 from player import Player
 from team import Team
+from random import random
+from math import floor
+from copy import deepcopy
 
 
 class Shuffler:
@@ -8,26 +11,57 @@ class Shuffler:
         self.players = players
         self.number_of_teams = number_of_teams
 
-    def shuffle(self):
+    def shuffle(self, iterations=10):
         """Receive players and returns balaced teams. The shuffle algorithm will try
 to balance all the all the skills in both teams."""
-        self.teamA = Team(self.players)
-        self.teamB = Team(self.players)
-        pass
 
-    def swap_player(self, team1, team2, player_index_t1, player_index_t2):
-        player_from_t1 = team1.players.pop(player_index_t1)
-        player_from_t2 = team2.players.pop(player_index_t2)
+        for i in range(1, iterations):
+            s0_t1, s0_t2, s0_p1_index, s0_p2_index = self.get_swap_candidate()
+            s1_t1, s1_t2, s1_p1_index, s1_p2_index = self.get_swap_candidate()
+            s2_t1, s2_t2, s2_p1_index, s2_p2_index = self.get_swap_candidate()
 
-        team1.players.append(player_from_t2)
-        team2.players.append(player_from_t1)
+            swap_effect_0 = self.calculate_swap_effect(s0_t1, s0_t2,
+                                                       s0_p1_index,
+                                                       s0_p2_index)
+            swap_effect_1 = self.calculate_swap_effect(s1_t1, s1_t2,
+                                                       s1_p1_index,
+                                                       s1_p2_index)
+            swap_effect_2 = self.calculate_swap_effect(s2_t1, s2_t2,
+                                                       s2_p1_index,
+                                                       s2_p2_index)
+
+            swap_array = [swap_effect_0, swap_effect_1, swap_effect_2]
+            swap_index = swap_array.index(min(swap_array))
+
+            if swap_index == 0:
+                swap_player(s0_t1, s0_t2, s0_p1_index, s0_p2_index)
+            elif swap_index == 1:
+                swap_player(s1_t1, s1_t2, s1_p1_index, s1_p2_index)
+            else:
+                swap_player(s2_t1, s2_t2, s2_p1_index, s2_p2_index)
+
+    def get_swap_candidate(self):
+        """() => [Team, Team, int, int]. Get candidate arguments to be used in
+swap_player."""
+        team1, team2 = self.get_random_team_pair()
+        p1_index = floor(random() * len(team1.players))
+        p2_index = floor(random() * len(team2.players))
+
+        return [team1, team2, p1_index, p2_index]
+
+    def calculate_swap_effect(self, team1, team2,
+                              player_index_t1, player_index_t2):
+        """(Team, Team, int, int) => float. Calculates the effect of
+swapping players. Negative is good."""
+        diff_before = self.calculate_team_diff()
+        t1 = deepcopy(team1)
+        t2 = deepcopy(team2)
+        swap_player(t1, t2, player_index_t1, player_index_t2)
+        diff_after = calculate_team_diff([t1, t2])
+        return diff_after - diff_before
 
     def get_team_skillsets(self):
-        teams_skills = [[] for _ in range(self.number_of_teams)]
-        for t in range(self.number_of_teams):
-            for skill in Player.skills:
-                teams_skills[t].append(self.teams[t].get_team_skill(skill))
-        return teams_skills
+        return get_team_skillsets(self.teams)
 
     def get_player_skillsets(self, players):
         number_of_players = len(players)
@@ -56,6 +90,11 @@ to balance all the all the skills in both teams."""
         for i in range(self.number_of_teams):
             self.teams.append(Team('Team' + str(i), teams[i]))
 
+    def get_random_team_pair(self):
+        team_idx_pair = get_random_index_pair(len(self.teams))
+        team_pair = map(lambda x: self.teams[x], team_idx_pair)
+        return list(team_pair)
+
 
 def calculate_error(v1, v2):
     return (v1 - v2) ** 2
@@ -70,3 +109,38 @@ def calculate_error_by_skillsets(skillsets):
                 error += calculate_error(skillsets[t1][s],
                                          skillsets[t2][s])
     return error
+
+
+def get_random_index_pair(array_length):
+    index_pair = []
+    while (len(index_pair) < 2):
+        index = floor(random() * array_length)
+        if (len(index_pair) == 0 or (len(index_pair) == 1
+                                     and index_pair[0] != index)):
+            index_pair.append(index)
+
+    return index_pair
+
+
+def get_team_skillsets(teams):
+    num_teams = len(teams)
+    teams_skills = [[] for _ in range(num_teams)]
+    for t in range(num_teams):
+        for skill in Player.skills:
+            teams_skills[t].append(teams[t].get_team_skill(skill))
+    return teams_skills
+
+
+def swap_player(team1, team2, player_index_t1, player_index_t2):
+    """(Team, Team, int, int) => void. Swaps player with player_index_t1 in team1
+with player with player_index_t2 in team2."""
+    player_from_t1 = team1.players.pop(player_index_t1)
+    player_from_t2 = team2.players.pop(player_index_t2)
+
+    team1.players.append(player_from_t2)
+    team2.players.append(player_from_t1)
+
+
+def calculate_team_diff(teams):
+    teams_skillsets = get_team_skillsets(teams)
+    return calculate_error_by_skillsets(teams_skillsets)
--- shuffle_test.py
+++ shuffle_test.py
@@ -1,7 +1,10 @@
 import unittest
 import shuffle
 from math import floor
+from team import Team
+from player import Player
 from player_data import get_players
+from random import random
 
 
 class ShufflerData(unittest.TestCase):
@@ -136,9 +139,9 @@ class SwapPlayer(SimpleShufflerTestCase):
         t0_player_name = team0.players[t0_player_index].name
         t1_player_name = team1.players[t1_player_index].name
 
-        self.shuffler.swap_player(team0, team1,
-                                  t0_player_index,
-                                  t1_player_index)
+        shuffle.swap_player(team0, team1,
+                            t0_player_index,
+                            t1_player_index)
 
         self.assertEqual(t0_player_name,
                          team1.players[-1].name,
@@ -146,6 +149,57 @@ class SwapPlayer(SimpleShufflerTestCase):
         self.assertEqual(t1_player_name,
                          team0.players[-1].name,
                          'Swap failed')
+
+
+class GetSwapCandidate(SimpleShufflerTestCase):
+    def runTest(self):
+        team1, team2, p_idx_1, p_idx_2 = self.shuffler.get_swap_candidate()
+
+        self.assertIsInstance(team1, Team, 'Getting swap candidate failed')
+        self.assertIsInstance(team1.players[p_idx_1], Player,
+                              'Getting swap candidate failed')
+        self.assertIsInstance(team2, Team, 'Getting swap candidate failed')
+        self.assertIsInstance(team2.players[p_idx_2], Player,
+                              'Getting swap candidate failed')
+
+
+class CalculateSwapEffect(SimpleShufflerTestCase):
+    def runTest(self):
+        team1, team2, p_idx_1, p_idx_2 = self.shuffler.get_swap_candidate()
+        make_team_strong(team1, 100)
+
+        swap_effect = (self.shuffler
+                       .calculate_swap_effect(team1, team2, p_idx_1, p_idx_2))
+        # team1.players[p_idx_1].print_score()
+        # team2.players[p_idx_2].print_score()
+        self.assertIsInstance(swap_effect, float,
+                              'Swap effect wrong result type')
+        self.assertTrue(swap_effect < 0, 'Swap effect should be negative')
+
+
+class Shuffle(SimpleShufflerTestCase):
+    def runTest(self):
+        team_0 = self.shuffler.teams[0]
+        team_1 = self.shuffler.teams[1]
+
+        make_team_strong(team_0, 500)
+        generate_random_state(self.players, 1000)
+
+        # team_0.print_all_skills()
+        # team_1.print_all_skills()
+
+        print('diff before: ' + str(self.shuffler.calculate_team_diff()))
+        self.shuffler.shuffle(100)
+
+        team_0.print_all_skills()
+        team_1.print_all_skills()
+
+        # for team in self.shuffler.teams:
+        #     print(team.name)
+        #     for player in team.players:
+        #         player.print_score()
+
+        print('diff after: ' + str(self.shuffler.calculate_team_diff()))
 
 
 def expected_players_by_team(number_of_players, number_of_teams):
@@ -164,6 +218,31 @@ def make_team_strong(team, diff):
         for skill in player.skills:
             currentValue = getattr(player, skill)
             setattr(player, skill, currentValue + diff)
+
+
+def generate_random_state(players, iterations):
+    for i in range(1, iterations):
+        generate_random_game(players)
+
+
+def get_random_player(players):
+    index = floor(random() * len(players))
+    return players[index]
+
+
+def get_random_skill():
+    index = floor(random() * len(Player.skills))
+    return Player.skills[index]
+
+
+def generate_random_game(players):
+    player1 = get_random_player(players)
+    player2 = get_random_player(players)
+
+    random_score = random()
+
+    if (player1 != player2):
+        player1.update_skill_level(player2, get_random_skill(), random_score)
 
 
 if __name__ == '__main__':
